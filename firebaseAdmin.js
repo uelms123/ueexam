9335abad6467995e1122891fd8058b15ca98be3d
uelms123 <uelms2025@gmail.com>
2025-10-08 12:57:17 +0530

    Add files via upload

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -3,7 +3,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "ueexam-1835a.firebasestorage.app"
+  storageBucket: "ueexam-6f7d8.firebasestorage.app"
 });
 
 module.exports = admin;
